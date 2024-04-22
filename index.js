4a19b4b85dc9cdf197d5928df6ca749876699fa8
Muzamilakram <muzamilakram789@gmail.com>
2024-04-22 20:09:57 +0500

    Adding a Word-Counter-Project

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bgMagenta.bold("\n\tWelcome to 'Muzamil' Word-Counter-Project\n"));
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.bgCyan("Enter your sentence to count the word:")
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+console.log(chalk.bgBlack(words));
+console.log(chalk.green(`Your sentence word count is ${words.length}`));
